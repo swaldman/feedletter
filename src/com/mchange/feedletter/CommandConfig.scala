b25093f479a167d042e60a02d389a8e62adf1b5b
Steve Waldman <swaldman@mchange.com>
2024-01-06 04:26:18 -0500

    Implement daemon reload following (in db) config changes, daemon resilience to transient db downtime.

--- src/com/mchange/feedletter/CommandConfig.scala
+++ src/com/mchange/feedletter/CommandConfig.scala
@@ -240,16 +240,34 @@ object CommandConfig extends SelfLogging:
       end zcommand
   end Db
   case object Daemon extends CommandConfig:
+      val ReloadCheckPeriod = 30.seconds // XXX: hard-coded for now
+      val RetrySchedule = Schedule.exponential( 10.seconds, 1.25f ) || Schedule.fixed( 1.minute ) // XXX: hard-coded for now
       override def zcommand : ZCommand =
-        for
-          as <- ZIO.service[AppSetup]
-          ds <- ZIO.service[DataSource]
-          tapirApi <- com.mchange.feedletter.Daemon.tapirApi( ds, as )
-          _  <- com.mchange.feedletter.Daemon.cyclingRetryingUpdateAssignComplete( ds, tapirApi ).fork
-          _  <- com.mchange.feedletter.Daemon.cyclingRetryingMailNextGroupIfDue( ds, as.smtpContext ).fork
-          _  <- com.mchange.feedletter.Daemon.webDaemon(ds, as, tapirApi)
-          _  <- ZIO.never
-        yield ()
+        def mustReloadCheck(ds : DataSource) =
+          for
+            _          <- ZIO.sleep(ReloadCheckPeriod)
+            mustReload <- PgDatabase.checkMustReloadTapirApi( ds )
+          yield
+            mustReload
+        val singleLoad =
+          for
+            as         <- ZIO.service[AppSetup]
+            ds         <- ZIO.service[DataSource]
+            _          <- PgDatabase.clearMustReloadTapirApi(ds)
+            tapirApi   <- com.mchange.feedletter.Daemon.tapirApi(ds,as)
+            _          <- INFO.zlog( s"Spawning daemon fibers." )
+            fuac       <- com.mchange.feedletter.Daemon.cyclingRetryingUpdateAssignComplete( ds, tapirApi ).fork
+            fmngid     <- com.mchange.feedletter.Daemon.cyclingRetryingMailNextGroupIfDue( ds, as.smtpContext ).fork
+            fwd        <- com.mchange.feedletter.Daemon.webDaemon( ds, as, tapirApi ).fork
+            _          <- ZIO.unit.schedule( Schedule.recurUntilZIO( _ =>  mustReloadCheck(ds).orDie ) )
+            _          <- INFO.zlog( s"Flag ${Flag.MustReloadTapirApi} found. Shutting down daemon and restarting." )
+            _          <- fuac.interrupt
+            _          <- fmngid.interrupt
+            _          <- fwd.interrupt
+            _          <- DEBUG.zlog("All daemon fibers interrupted.")
+          yield ()
+        singleLoad.resurrect.retry( RetrySchedule ) // if we have database problems, keep trying to reconnect
+          .schedule( Schedule.forever )             // a successful completion signals a reload request. so we restart
       end zcommand
   object Style:
     case class ComposeUntemplateSingle(
