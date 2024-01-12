6b28c00b7981edb053129f09baed18d8de842ef1
Steve Waldman <swaldman@mchange.com>
2024-01-12 15:25:09 -0500

    Rename default e-mail compose untemplates in unstatic ( xxx.html.untemplate ) style.

--- src/com/mchange/feedletter/Default.scala
+++ src/com/mchange/feedletter/Default.scala
@@ -25,8 +25,8 @@ object Default:
     val WebApiPort            : Option[Int] = None
 
   object Email:
-    val ComposeUntemplateSingle   = "com.mchange.feedletter.default.email.composeUniversal"
-    val ComposeUntemplateMultiple = "com.mchange.feedletter.default.email.composeUniversal"
-    val ConfirmUntemplate         = "com.mchange.feedletter.default.email.confirm"
-    val StatusChangeUntemplate    = "com.mchange.feedletter.default.email.statusChange"
+    val ComposeUntemplateSingle   = "com.mchange.feedletter.default.email.composeUniversal_html"
+    val ComposeUntemplateMultiple = "com.mchange.feedletter.default.email.composeUniversal_html"
+    val ConfirmUntemplate         = "com.mchange.feedletter.default.email.confirm_html"
+    val StatusChangeUntemplate    = "com.mchange.feedletter.default.email.statusChange_html"
 
