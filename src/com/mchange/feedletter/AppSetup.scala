5f04af5d04e512636e67d8ff97249bd07b4db4d4
Steve Waldman <swaldman@mchange.com>
2024-01-28 01:09:30 -0500

    If we know the location of our PID file, and it matches our actual process ID, remove it on shutdown.

--- src/com/mchange/feedletter/AppSetup.scala
+++ src/com/mchange/feedletter/AppSetup.scala
@@ -5,12 +5,15 @@ import java.io.{BufferedInputStream,InputStream}
 import java.nio.file.{Path as JPath}
 import java.util.Properties
 import scala.util.Using
+import scala.util.control.NonFatal
 import scala.jdk.CollectionConverters.*
 
 import com.mchange.mailutil.Smtp
 import com.mchange.conveniences.javautil.*
 
-object AppSetup:
+import MLevel.*
+
+object AppSetup extends SelfLogging:
 
   val AcceptableSecretsPermStrings = Set("r--------","rw-------")
   val AcceptableSecretsPerms = AcceptableSecretsPermStrings.map( os.PermSet.fromString )
@@ -34,9 +37,33 @@ object AppSetup:
       java.util.logging.LogManager.getLogManager().readConfiguration( is )
     source
 
+  def setupAutoremovePidFile() : UIO[Unit] =
+    val risky =
+      ZIO.attempt:
+        sys.env.get("MILL_DAEMON_PID_FILE").foreach: pidFileLoc =>
+          val pidFilePath = os.Path( pidFileLoc )
+          val onShutdown =
+            new Thread:
+              override def run() : Unit =
+                try
+                  if os.exists(pidFilePath) then
+                    val myPid = ProcessHandle.current().pid()
+                    val fromFilePid = os.read(pidFilePath).trim().toLong
+                    if myPid == fromFilePid then
+                      INFO.log(s"Shutdown Hook: Removing PID file '${pidFilePath}'")
+                      os.remove( pidFilePath )
+                catch
+                  case NonFatal(t) => WARNING.log("Throwable while executing autoremove PID file shutdown hook.", t)
+          java.lang.Runtime.getRuntime().addShutdownHook(onShutdown)
+    risky.catchAll: t =>
+      t match
+        case NonFatal(t) => ZIO.succeed(WARNING.log("Throwable while setting up autoremove PID file shutdown hook.", t))
+        case other => throw other
+
   def live( secrets : Option[JPath] ) : ZLayer[Any, Throwable, AppSetup] = ZLayer.fromZIO:
     for
       lcfg <- julAppSetup
+      _    <- setupAutoremovePidFile()
     yield
       val (loc : Option[os.Path], props : Properties) =
         secrets match
