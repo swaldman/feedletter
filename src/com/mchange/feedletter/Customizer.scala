b0214c9682e20b60bb974d74557b20687c839a12
Steve Waldman <swaldman@mchange.com>
2024-06-21 11:24:44 -0400

    More concise Customizer definitions.

--- src/com/mchange/feedletter/Customizer.scala
+++ src/com/mchange/feedletter/Customizer.scala
@@ -11,13 +11,12 @@ import java.time.ZoneId
 import MLevel.*
 
 object Customizer extends SelfLogging:
-  type Subject = ( subscribableName : SubscribableName, subscriptionManager : SubscriptionManager, feedUrl : FeedUrl, contents : Seq[ItemContent], timeZone : ZoneId ) => String
-  type Contents = ( subscribableName : SubscribableName, subscriptionManager : SubscriptionManager, feedUrl : FeedUrl, contents : Seq[ItemContent], timeZone : ZoneId  ) => Seq[ItemContent]
-  type MastoAnnouncement = ( subscribableName : SubscribableName, subscriptionManager : SubscriptionManager, feedUrl : FeedUrl, content : ItemContent, timeZone : ZoneId ) => Option[String]
-  type TemplateParams =
-    ( subscribableName : SubscribableName, subscriptionManager : SubscriptionManager, feedUrl : FeedUrl, destination : Destination, subscriptionId : SubscriptionId, removeLink : String ) => Map[String,String]
-  type Filter = ( subscribableName : SubscribableName, subscriptionManager : SubscriptionManager, content : ItemContent ) => Boolean
-  type HintAnnounceRestriction = ( subscribableName : SubscribableName, subscriptionManager : SubscriptionManager, content : ItemContent ) => Option[Iffy.HintAnnounce.Policy]
+  type Subject                 = ( SubscribableName, SubscriptionManager, FeedUrl, Seq[ItemContent], ZoneId )            => String
+  type Contents                = ( SubscribableName, SubscriptionManager, FeedUrl, Seq[ItemContent], ZoneId )            => Seq[ItemContent]
+  type MastoAnnouncement       = ( SubscribableName, SubscriptionManager, FeedUrl, ItemContent, ZoneId )                 => Option[String]
+  type TemplateParams          = ( SubscribableName, SubscriptionManager, FeedUrl, Destination, SubscriptionId, String ) => Map[String,String]
+  type Filter                  = ( SubscribableName, SubscriptionManager, ItemContent )                                  => Boolean
+  type HintAnnounceRestriction = ( SubscribableName, SubscriptionManager, ItemContent )                                  => Option[Iffy.HintAnnounce.Policy]
 
   object Subject                 extends Registry[Customizer.Subject]
   object Contents                extends Registry[Customizer.Contents]
