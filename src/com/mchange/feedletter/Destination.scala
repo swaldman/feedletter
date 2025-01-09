5f58af3c12c16ab0918bcac7a9730a0afcebfa5c
Steve Waldman <swaldman@mchange.com>
2025-01-09 07:13:55 -0500

    Use Destination.Key more explicitly.

--- src/com/mchange/feedletter/Destination.scala
+++ src/com/mchange/feedletter/Destination.scala
@@ -38,9 +38,7 @@ object Destination:
 
   private enum Key:
     def s = this.toString
-    case addressPart,displayNamePart,number,name,instanceUrl,version,`type`,destinationType,entrywayUrl,identifier
-
-  import Key.*
+    case addressPart,displayNamePart,address,displayName,number,name,instanceUrl,version,`type`,destinationType,entrywayUrl,identifier
 
   object Email:
     def apply( address : Smtp.Address ) : Email = Email( address.email, address.displayName )
@@ -49,7 +47,7 @@ object Destination:
     lazy val toAddress : Smtp.Address = Smtp.Address( addressPart, displayNamePart )
     lazy val rendered = toAddress.rendered
     override def unique = s"e-mail:${addressPart}"
-    override def toFields = Seq( destinationType.s -> Tag.Email.toString, Key.addressPart.s -> this.addressPart) ++ this.displayNamePart.map( dnp => Key.displayNamePart.s -> dnp )
+    override def toFields = Seq( Key.destinationType.s -> Tag.Email.toString, Key.addressPart.s -> this.addressPart) ++ this.displayNamePart.map( dnp => Key.displayNamePart.s -> dnp )
     override def shortDesc : String = this.addressPart 
     override def fullDesc : String = this.rendered
     override def defaultDesc : String = fullDesc
@@ -57,21 +55,21 @@ object Destination:
     override lazy val whyInvalid : Option[Throwable] = this.toAddress.whyInvalid
   case class Mastodon( name : String, instanceUrl : String ) extends Destination:
     override def unique = "mastodon:" + wwwFormEncodeUTF8((Key.name.s,name),(Key.instanceUrl.s,instanceUrl))
-    override def toFields = Seq( destinationType.s -> Tag.Mastodon.toString, Key.name.s -> this.name, Key.instanceUrl.s -> this.instanceUrl )
+    override def toFields = Seq( Key.destinationType.s -> Tag.Mastodon.toString, Key.name.s -> this.name, Key.instanceUrl.s -> this.instanceUrl )
     override def shortDesc : String = this.instanceUrl
     override def fullDesc : String = s"Mastodon instance nicknamed '${name}' at ${instanceUrl}"
     override def defaultDesc : String = shortDesc
     override def toCsvRow : Seq[String] = Seq( instanceUrl, q(name) )
   case class Sms( number : String ) extends Destination:
     override def unique = s"sms:${number}"
-    override def toFields = Seq( destinationType.s -> Tag.Sms.toString, Key.number.s -> this.number )
+    override def toFields = Seq( Key.destinationType.s -> Tag.Sms.toString, Key.number.s -> this.number )
     override def shortDesc : String = this.number
     override def fullDesc : String = s"SMS destination '${number}'"
     override def defaultDesc : String = shortDesc
     override def toCsvRow : Seq[String] = Seq( q(number) )
   case class BlueSky( entrywayUrl : String, identifier : String  ) extends Destination:
     override def unique = "bluesky:" + wwwFormEncodeUTF8((Key.entrywayUrl.s,entrywayUrl),(Key.identifier.s,identifier))
-    override def toFields = Seq( destinationType.s -> Tag.BlueSky.toString, Key.entrywayUrl.s -> this.entrywayUrl, Key.identifier.s -> this.identifier )
+    override def toFields = Seq( Key.destinationType.s -> Tag.BlueSky.toString, Key.entrywayUrl.s -> this.entrywayUrl, Key.identifier.s -> this.identifier )
     override def shortDesc : String = s"${identifier} at ${entrywayUrl}"
     override def fullDesc : String = s"Bluesky account identified as '${identifier}' at entryway '${entrywayUrl}'"
     override def defaultDesc : String = shortDesc
@@ -97,27 +95,27 @@ object Destination:
           ap <- fmap.get(kap)
         yield
           fmap.get(kdnp).fold( Email(Smtp.Address.parseSingle(ap)) )( dnp => Email( addressPart=ap, displayNamePart=Some(dnp) ) )
-      def fromActualFields = fromFieldPair(addressPart.s,displayNamePart.s)
-      def fromFriendlierFields = fromFieldPair("address","displayName")
+      def fromActualFields = fromFieldPair(Key.addressPart.s,Key.displayNamePart.s)
+      def fromFriendlierFields = fromFieldPair(Key.address.s,Key.displayName.s)
       fromActualFields orElse fromFriendlierFields
     def mastodon( fields : Seq[(String,String)] ) : Option[Destination.Mastodon] = mastodon( CarefulMap( fields ) )
     private def mastodon( fmap : CarefulMap ) : Option[Destination.Mastodon] =
       for
-        nm <- fmap.get( name.s )
-        iu <- fmap.get( instanceUrl.s )
+        nm <- fmap.get( Key.name.s )
+        iu <- fmap.get( Key.instanceUrl.s )
       yield
         Mastodon( name = nm, instanceUrl = iu )
     def sms( fields : Seq[(String,String)] ) : Option[Destination.Sms] = sms( CarefulMap( fields ) )
     private def sms( fmap : CarefulMap ) : Option[Destination.Sms] =
       for
-        nu <- fmap.get( number.s )
+        nu <- fmap.get( Key.number.s )
       yield
         Sms( number = nu )
     def bluesky( fields : Seq[(String,String)] ) : Option[Destination.BlueSky] = bluesky( CarefulMap( fields ) )
     private def bluesky( fmap : CarefulMap ) : Option[Destination.BlueSky] =
       for
-        eu <- fmap.get( entrywayUrl.s )
-        id <- fmap.get( identifier.s )
+        eu <- fmap.get( Key.entrywayUrl.s )
+        id <- fmap.get( Key.identifier.s )
       yield
         BlueSky( entrywayUrl = eu, identifier = id )
     private def byType( tpe : String, fmap : CarefulMap ) : Option[Destination] =
@@ -129,7 +127,7 @@ object Destination:
         case None                 => None
     def apply( fields : Seq[(String,String)] ) : Option[Destination] = apply( CarefulMap(fields) )
     private def apply( fmap : CarefulMap ) : Option[Destination] =
-      val tpe = fmap.get(destinationType.s) orElse fmap.get(`type`.s)
+      val tpe = fmap.get(Key.destinationType.s) orElse fmap.get(Key.`type`.s)
       tpe match
         case Some( t ) => byType(t,fmap)
         case None =>
@@ -140,10 +138,10 @@ object Destination:
             case n => throw new AmbiguousDestination( s"""Fields '${fmap.fields.mkString(", ")}' can be interpreted as multiple ($n) Destinations: ${destinations.mkString(", ")}""" )
 
   private def toUJsonV1( destination : Destination ) : ujson.Value =
-    def emf( email : Destination.Email )   : ujson.Obj = ujson.Obj.from(Seq(addressPart.s->ujson.Str(email.addressPart)) ++ email.displayNamePart.map(dnp=>(displayNamePart.s->ujson.Str(dnp))))
-    def smsf( sms : Destination.Sms )      : ujson.Obj = ujson.Obj( number.s -> sms.number )
-    def mf( masto : Destination.Mastodon ) : ujson.Obj = ujson.Obj( name.s -> masto.name, instanceUrl.s -> masto.instanceUrl )
-    def bsf( bsky : Destination.BlueSky )  : ujson.Obj = ujson.Obj( entrywayUrl.s -> bsky.entrywayUrl, identifier.s -> bsky.identifier )
+    def emf( email : Destination.Email )   : ujson.Obj = ujson.Obj.from(Seq(Key.addressPart.s->ujson.Str(email.addressPart)) ++ email.displayNamePart.map(dnp=>(Key.displayNamePart.s->ujson.Str(dnp))))
+    def smsf( sms : Destination.Sms )      : ujson.Obj = ujson.Obj( Key.number.s -> sms.number )
+    def mf( masto : Destination.Mastodon ) : ujson.Obj = ujson.Obj( Key.name.s -> masto.name, Key.instanceUrl.s -> masto.instanceUrl )
+    def bsf( bsky : Destination.BlueSky )  : ujson.Obj = ujson.Obj( Key.entrywayUrl.s -> bsky.entrywayUrl, Key.identifier.s -> bsky.identifier )
     val (fields, tpe) =
       destination match
         case email : Destination.Email    => (emf(email), Tag.Email)
@@ -152,8 +150,8 @@ object Destination:
         case bsky  : Destination.BlueSky  => (bsf(bsky),  Tag.BlueSky)
     val headerFields =
        ujson.Obj(
-         version.s -> 1,
-         `type`.s -> tpe.toString
+         Key.version.s -> 1,
+         Key.`type`.s -> tpe.toString
        )
     ujson.Obj.from(headerFields.obj ++ fields.obj)
 
@@ -163,12 +161,12 @@ object Destination:
     if version.nonEmpty && version != Some(1) then
       throw new InvalidDestination(s"Unpickling Destination, found version ${version.get}, expected version 1: " + obj.mkString(", "))
     else
-      val tpe = obj(`type`.s).str
+      val tpe = obj(Key.`type`.s).str
       Tag.valueOf(tpe) match
-        case Tag.Email    => Destination.Email( addressPart = obj(addressPart.s).str, displayNamePart = obj.get(displayNamePart.s).map(_.str)) 
-        case Tag.Sms      => Destination.Sms( number = obj(number.s).str )
-        case Tag.Mastodon => Destination.Mastodon( name = obj(name.s).str, instanceUrl = obj(instanceUrl.s).str )
-        case Tag.BlueSky  => Destination.BlueSky( entrywayUrl = obj(entrywayUrl.s).str, identifier = obj(identifier.s).str )
+        case Tag.Email    => Destination.Email( addressPart = obj(Key.addressPart.s).str, displayNamePart = obj.get(Key.displayNamePart.s).map(_.str)) 
+        case Tag.Sms      => Destination.Sms( number = obj(Key.number.s).str )
+        case Tag.Mastodon => Destination.Mastodon( name = obj(Key.name.s).str, instanceUrl = obj(Key.instanceUrl.s).str )
+        case Tag.BlueSky  => Destination.BlueSky( entrywayUrl = obj(Key.entrywayUrl.s).str, identifier = obj(Key.identifier.s).str )
 
   given ReadWriter[Destination] =
     readwriter[ujson.Value].bimap[Destination](
