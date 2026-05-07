963ecb19a292ed1490d7996bea3fdff4cef6dd7c
Steve Waldman <swaldman@mchange.com>
2026-05-07 18:39:15 -0300

    Factor out trivialtemplate; update for 0.2.3-SNAPSHOT.

--- src/com/mchange/feedletter/trivialtemplate/exception.scala
+++
@@ -1,6 +0,0 @@
-package com.mchange.feedletter.trivialtemplate
-
-import com.mchange.feedletter.FeedletterException
-
-class KeyNotFound( msg : String, cause : Throwable = null ) extends FeedletterException( msg, cause )
-
